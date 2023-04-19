48653288c276e5493713c06df6eee6ec16079edf
Seinu <102974491+Seinuve@users.noreply.github.com>
2023-04-19 13:31:18 -0600

    1.4.1 push
    here

--- Kudasai.py
+++ Kudasai.py
@@ -115,7 +115,7 @@ def check_update():
 
     try:
     
-        CURRENT_VERSION = "v1.4.0" ## hardcoded current vers
+        CURRENT_VERSION = "v1.4.1" ## hardcoded current vers
 
         response = requests.get("https://api.github.com/repos/Seinuve/Kudasai/releases/latest")
         latestVersion = response.json()["tag_name"]

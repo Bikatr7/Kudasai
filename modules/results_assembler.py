56a0c4d71033ab92f797a6f3e95a1a8368726b80
Bikatr7 <Tetralon07@gmail.com>
2023-11-08 11:22:13 -0700

    refactored results_assembler

--- modules/results_assembler.py
+++ modules/results_assembler.py
@@ -24,102 +24,98 @@ class ResultsAssembler:
 
 ##-------------------start-of-write_kaiseki_results()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def write_kaiseki_results(self, kaiseki_client:Kaiseki) -> None:
+    @staticmethod
+    def write_kaiseki_results(kaiseki_client:Kaiseki) -> None:
 
         """
 
         This function is called to write the results of the Kaiseki translation module to the output directory.
 
         Parameters:
-        self (object - preloader) : the preloader object.
+        FileEnsurer (object - preloader) : the preloader object.
         kaiseki_client (object - Kaiseki) : the Kaiseki object.
 
         """
 
         ## ensures the output directory exists, cause it could get moved or fucked with.
-        self.file_handler.standard_create_directory(self.file_handler.output_dir)
+        FileEnsurer.standard_create_directory(FileEnsurer.output_dir)
 
-        with open(self.error_log_path, 'a+', encoding='utf-8') as file:
+        with open(FileEnsurer.error_log_path, 'a+', encoding='utf-8') as file:
             file.writelines(kaiseki_client.error_text) ## type: ignore (we know it's not None)
 
-        with open(self.je_check_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.je_check_path, 'w', encoding='utf-8') as file:
             file.writelines(kaiseki_client.je_check_text) ## type: ignore (we know it's not None)
 
-        with open(self.translated_text_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.translated_text_path, 'w', encoding='utf-8') as file:
             file.writelines(kaiseki_client.translated_text) ## type: ignore (we know it's not None)
 
         ## pushes the tl debug log to the file
-        self.file_handler.logger.clear_log_file()
-        self.file_handler.logger.push_batch()
+        Logger.clear_log_file()
+        Logger.push_batch()
 
 ##-------------------start-of-write_kijiku_results()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def write_kijiku_results(self, kijiku_client:Kijiku) -> None:
+    @staticmethod
+    def write_kijiku_results(kijiku_client:Kijiku) -> None:
 
         """
         
-        This function is called to write the results of the Kijiku translation module to the output directory.\n
+        This function is called to write the results of the Kijiku translation module to the output directory.
 
-        Parameters:\n
-        self (object - preloader) : the preloader object.\n
-        kijiku_client (object - Kijiku) : the Kijiku object.\n
-
-        Returns:\n
-        None.\n
+        Parameters:
+        kijiku_client (object - Kijiku) : the Kijiku object.
 
         """
 
         ## ensures the output directory exists, cause it could get moved or fucked with.
-        self.file_handler.standard_create_directory(self.file_handler.output_dir)
+        FileEnsurer.standard_create_directory(FileEnsurer.output_dir)
 
-        with open(self.error_log_path, 'a+', encoding='utf-8') as file:
+        with open(FileEnsurer.error_log_path, 'a+', encoding='utf-8') as file:
             file.writelines(kijiku_client.error_text) ## type: ignore (we know it's not None)
 
-        with open(self.je_check_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.je_check_path, 'w', encoding='utf-8') as file:
             file.writelines(kijiku_client.je_check_text) ## type: ignore (we know it's not None)
 
-        with open(self.translated_text_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.translated_text_path, 'w', encoding='utf-8') as file:
             file.writelines(kijiku_client.translated_text) ## type: ignore (we know it's not None)
 
         ## pushes the tl debug log to the file
-        self.file_handler.logger.clear_log_file()
-        self.file_handler.logger.push_batch()
+        Logger.clear_log_file()
+        Logger.push_batch()
 
 ##-------------------start-of-write_kairyou_results()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def write_kairyou_results(self, kairyou_client:Kairyou) -> None:
+    @staticmethod
+    def write_kairyou_results(kairyou_client:Kairyou) -> None:
 
         """
         
-        This function is called to write the results of the preprocessing module to the output directory.\n
+        This function is called to write the results of the preprocessing module to the output directory.
 
-        Parameters:\n
-        self (object - preloader) : the preloader object.\n
-        kairyou_client (object - Kairyou) : the Kairyou object.\n
-
-        Returns:\n
-        None.\n
+        Parameters:
+        FileEnsurer (object - preloader) : the preloader object.
+        kairyou_client (object - Kairyou) : the Kairyou object.
 
         """
 
         ## ensures the output directory exists, cause it could get moved or fucked with.
-        self.file_handler.standard_create_directory(self.file_handler.output_dir)
+        FileEnsurer.standard_create_directory(FileEnsurer.output_dir)
 
-        with(open(self.preprocessed_text_path, 'w', encoding='utf-8')) as file:
+        with(open(FileEnsurer.preprocessed_text_path, 'w', encoding='utf-8')) as file:
             file.write(kairyou_client.text_to_preprocess) ## type: ignore (we know it's not None)
 
-        with open(self.kairyou_log_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.kairyou_log_path, 'w', encoding='utf-8') as file:
             file.write(kairyou_client.preprocessing_log) ## type: ignore (we know it's not None)
 
-        with open(self.error_log_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.error_log_path, 'w', encoding='utf-8') as file:
             file.write(kairyou_client.error_log) ## type: ignore (we know it's not None)
 
-        with open(self.je_check_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.je_check_path, 'w', encoding='utf-8') as file:
             file.truncate()
 
-        with open(self.translated_text_path, 'w', encoding='utf-8') as file:
+        with open(FileEnsurer.translated_text_path, 'w', encoding='utf-8') as file:
             file.truncate()
 
         ## pushes the tl debug log to the file
-        self.file_handler.logger.clear_log_file()
-        self.file_handler.logger.push_batch()
+        Logger.clear_log_file()
+        Logger.push_batch()

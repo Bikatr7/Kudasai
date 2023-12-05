94cb44b26c99f33605957cdd1590e6f66416019a
Bikatr7 <Tetralon07@gmail.com>
2023-12-05 16:10:15 -0700

    turns out gradio can't do static lol
    Fixed to work, still unfinished

--- unfinished_gui.py
+++ unfinished_gui.py
@@ -7,56 +7,78 @@ class KudasaiGUI:
 
     """
     
-    KudasaiGUI is a class that contains the GUI for Kudasai.
+    Kudasai is a class that contains the GUI for Kudasai.
 
     """
 
-    interface: gr.Blocks
+##-------------------start-of-build_gui()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-##-------------------start-of-build_interface---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def build_gui(self):
 
-    @staticmethod
-    def build_interface():
-        with gr.Blocks() as KudasaiGUI.interface:
+        """
 
-            with gr.Tab("Kudasai"):  # Tab1
-                with gr.Tab('Kairyou + Kijiku') as KairyouTab:
+        Builds the GUI.
+
+        """
+
+        with gr.Blocks() as self.gui:
+
+            ## tab 1
+            with gr.Tab("Kudasai") as self.kudasai_tab:
+
+                ## tab 2
+                with gr.Tab("Preprocessing and Translation") as self.model_tab:
                     with gr.Row():
+
+                        ## input files
                         with gr.Column():
-                            self.Tab1_txt_file = gr.File(label='TXT file with Japanese Text', file_count='single', file_types=['.txt'], type='file')
-                            self.Tab1_json_file = gr.File(label='JSON file', file_count='single',
-                                                     file_types=['.json'], type='file')
-                            with gr.Row():
-                                self.Tab1_auto_TL_mode = gr.Radio(label='Auto-Translation', choices=['Preprocess only', 'AutoTL with Kijiku'], value='Preprocess only', type='index', interactive=True)
+                            self.input_txt_file = gr.File(label='TXT file with Japanese Text', file_count='single', file_types=['.txt'], type='file')
+                            self.input_json_file = gr.File(label='Replacements JSON file', file_count='single', file_types=['.json'], type='file')
 
+                            ## mode selection 
                             with gr.Row():
-                                self.Tab1_run_btn = gr.Button('Run')
-                                self.Tab1_clear_btn = gr.Button('Clear', variant='stop')
+                                self.mode_selection = gr.Radio(label='Mode', choices=['Preprocess only', 'AutoTL with OpenAI', 'AutoTL with DeepL'], value='Preprocess only', type='index', interactive=True)
 
+                            ## run and clear buttons
                             with gr.Row():
-                                self.Tab1_start_log = gr.Button('Start log')
-                                self.Tab1_stop_log = gr.Button('Stop Log', variant='stop')
+                                self.run_button = gr.Button('Run')
+                                self.clear_button = gr.Button('Clear', variant='stop')
 
+                        ## output fields
                         with gr.Column():
-                            self.Tab1_output1 = gr.Textbox(label='Pre-processed text', lines=15, max_lines=25, interactive=False)
+                            self.preprocess_output_field  = gr.Textbox(label='Preprocessed text', lines=27, show_label=True, interactive=False, show_copy_button=True)
 
                             with gr.Row():
-                                self.Tab1_copy_btn = gr.Button('Copy to Clipboard')
-                                self.Tab1_save_btn = gr.Button('Save As')
-                            # self.Tab1_send_to_kijiku = gr.Button('Send to Kijiku AutoTL')
-                    with gr.Row():
-                        self.Tab1_output2 = gr.Textbox(label='Results', lines=10, max_lines=25)
-                        self.Tab1_output4 = gr.Textbox(label='Error Log', lines=10, max_lines=25)
+                                self.save_to_file_one = gr.Button('Save As')
+                            
+                        with gr.Column():
+                            self.translation_output_field = gr.Textbox(label='Translated Text', lines=27, show_label=True, interactive=False, show_copy_button=True)
 
+                            with gr.Row():
+                                self.save_to_file_two = gr.Button('Save As')
+                ## tab 3
+                with gr.Tab("Logging") as self.results_tab:
                     with gr.Row():
-                        self.Tab1_output5 = gr.Textbox(label='Translation', lines=10, max_lines=25)
+                        self.preprocessing_results = gr.Textbox(label='Preprocessing Results', lines=10, interactive=False)
+                        self.debug_log = gr.Textbox(label='Debug Log', lines=10, interactive=False)
+                        self.error_log = gr.Textbox(label='Error Log', lines=10, interactive=False)
 
-                    with gr.Row():
-                        self.Tab1_output3 = gr.Textbox(label='Debug Log', lines=10, max_lines=25)
 
-    @staticmethod
-    def launch():
-        KudasaiGUI.interface.queue().launch(inbrowser=True, show_error=True, server_name="127.0.0.1")
+##-------------------start-of-launch()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------                
+
+    def launch(self):
+
+        """
+
+        Launches the GUI.
+
+        """
+
+        self.build_gui()
+        self.gui.queue().launch(inbrowser=True, show_error=True)
+
+##-------------------start-of-main()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if(__name__ == '__main__'):
-    KudasaiGUI.launch()
+    kudasai_gui = KudasaiGUI()
+    kudasai_gui.launch()

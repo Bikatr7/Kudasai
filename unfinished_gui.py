c6b59c7fe5269aabf2c6c8653ca75534acce1043
Bikatr7 <Tetralon07@gmail.com>
2023-12-06 13:44:32 -0700

    made var names clearer, added doc names and s prep to properly seperate tabs

--- unfinished_gui.py
+++ unfinished_gui.py
@@ -23,11 +23,11 @@ class KudasaiGUI:
 
         with gr.Blocks() as self.gui:
 
-            ## tab 1
+            ## tab 1 | Main
             with gr.Tab("Kudasai") as self.kudasai_tab:
 
-                ## tab 2
-                with gr.Tab("Preprocessing and Translation") as self.model_tab:
+                ## tab 2 | preprocessing
+                with gr.Tab("Preprocessing") as self.preprocessing_tab:
                     with gr.Row():
 
                         ## input files
@@ -35,34 +35,35 @@ class KudasaiGUI:
                             self.input_txt_file = gr.File(label='TXT file with Japanese Text', file_count='single', file_types=['.txt'], type='file')
                             self.input_json_file = gr.File(label='Replacements JSON file', file_count='single', file_types=['.json'], type='file')
 
-                            ## mode selection 
-                            with gr.Row():
-                                self.mode_selection = gr.Dropdown(label='Mode', choices=['Preprocess only', 'AutoTL with OpenAI', 'AutoTL with DeepL'], value='Preprocess only', type='index', interactive=True)
-
-                            with gr.Row():
-                                self.api_key_input = gr.Textbox(label='API Key (Not needed for preprocess)', lines=1, show_label=True, interactive=True, type='password')
+                            ##self.api_key_input = gr.Textbox(label='API Key (Not needed for preprocess)', lines=1, show_label=True, interactive=True, type='password')
 
                             ## run and clear buttons
                             with gr.Row():
-                                self.run_button = gr.Button('Run')
-                                self.clear_button = gr.Button('Clear', variant='stop')
+                                self.preprocessing_run_button = gr.Button('Run')
+                                self.preprocessing_clear_button = gr.Button('Clear', variant='stop')
 
                         ## output fields
                         with gr.Column():
-                            self.preprocess_output_field  = gr.Textbox(label='Preprocessed text', lines=33, show_label=True, interactive=False, show_copy_button=True)
+                            self.preprocess_output_field  = gr.Textbox(label='Preprocessed text', lines=22, max_lines=22, show_label=True, interactive=False, show_copy_button=True)
 
                             with gr.Row():
-                                self.save_to_file_one = gr.Button('Save As')
+                                self.save_to_file_preprocessed_text = gr.Button('Save As')
                             
                         with gr.Column():
-                            self.translation_output_field = gr.Textbox(label='Translated Text', lines=33, show_label=True, interactive=False, show_copy_button=True)
+                            self.preprocessing_results = gr.Textbox(label='Preprocessing Results', lines=22, interactive=False, show_copy_button=True)
 
                             with gr.Row():
-                                self.save_to_file_two = gr.Button('Save As')
-                ## tab 3
+                                self.save_to_file_preprocessing_results = gr.Button('Save As')
+
+                        with gr.Column():
+                            self.debug_log = gr.Textbox(label='Debug Log', lines=22, interactive=False, show_copy_button=True)
+
+                            with gr.Row():
+                                self.save_to_file_debug_log = gr.Button('Save As')
+
+
+                ## tab 3 | Logging
                 with gr.Tab("Logging") as self.results_tab:
-                    with gr.Row():
-                        self.preprocessing_results = gr.Textbox(label='Preprocessing Results', lines=10, interactive=False)
 
                     with gr.Row():
                         self.debug_log = gr.Textbox(label='Debug Log', lines=10, interactive=False)
@@ -70,19 +71,31 @@ class KudasaiGUI:
                     with gr.Row():
                         self.error_log = gr.Textbox(label='Error Log', lines=10, interactive=False)
 
+##-------------------start-of-preprocessing_run_button_click()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-            def run_button_click(filepath):
+            def preprocessing_run_button_click(filepath):
+
+                """
+
+                Runs the preprocessing.
+
+                Parameters:
+                filepath (str) : The path to the input file.
+
+                """
 
                 if(self.input_txt_file is not None):
 
-                    txt_file_path = filepath.name ## type: ignore
+                    txt_file_path = filepath.name ## type: ignore | name is not type hinting for some fucking reason
 
                     with open(txt_file_path, "r", encoding='utf-8') as file:
                         text = file.read()
 
                     return text
+                
+##-------------------start-of-Listeners---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-            self.run_button.click(fn=run_button_click, inputs=[self.input_txt_file], outputs=[self.preprocess_output_field])
+            self.preprocessing_run_button.click(fn=preprocessing_run_button_click, inputs=[self.input_txt_file], outputs=[self.preprocess_output_field])
 
 ##-------------------start-of-launch()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------                
 

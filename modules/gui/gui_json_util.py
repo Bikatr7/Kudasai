2575ea5587f6fba90a13e7b81be4761ea442124b
Bikatr7 <Tetralon07@gmail.com>
2023-12-17 17:26:57 -0700

    refactored gui_json_util.py into a static class

--- modules/gui/gui_json_util.py
+++ modules/gui/gui_json_util.py
@@ -4,23 +4,28 @@ import json
 ## custom modules
 from modules.common.file_ensurer import FileEnsurer
 
-##-
+class GuiJsonUtil:
 
-def fetch_kijiku_settings_tab_default_values(key_name:str) -> str:
-    
-    """
-    
-    Fetches the default values for the settings tab from the kijiku_settings.json file.
+    current_kijiku_rules = FileEnsurer.config_kijiku_rules_path
 
-    Parameters:
-    key_name (str) : Which value to fetch.
+##-------------------start-of-fetch_kijiku_settings_tab_default_values()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    Returns:
-    (str) : The default value for the specified key. 
+    @staticmethod
+    def fetch_kijiku_settings_tab_default_values(key_name:str) -> str:
+        
+        """
+        
+        Fetches the default values for the settings tab from the kijiku_settings.json file.
 
-    """
+        Parameters:
+        key_name (str) : Which value to fetch.
 
-    with open(FileEnsurer.config_kijiku_rules_path, "r") as f:
-        data = json.load(f)
+        Returns:
+        (str) : The default value for the specified key. 
 
-    return data["open ai settings"][key_name]
+        """
+
+        with open(GuiJsonUtil.current_kijiku_rules, "r") as file:
+            data = json.load(file)
+
+        return data["open ai settings"][key_name]

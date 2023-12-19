4ba347e4b461d8640d0395256af16ff4a5c419a2
Bikatr7 <Tetralon07@gmail.com>
2023-12-19 01:54:12 -0700

    Add method to update kijiku settings with new values

--- modules/gui/gui_json_util.py
+++ modules/gui/gui_json_util.py
@@ -1,8 +1,11 @@
 ## built-in libraries
 import json
+import typing
 
 ## custom modules
 from modules.common.file_ensurer import FileEnsurer
+
+from handlers.json_handler import JsonHandler
 
 class GuiJsonUtil:
 
@@ -29,3 +32,26 @@ class GuiJsonUtil:
             data = json.load(file)
 
         return data["open ai settings"][key_name]
+    
+##-------------------start-of-update_kijiku_settings_with_new_values()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def update_kijiku_settings_with_new_values(new_values:typing.List[typing.Tuple[str,str]]) -> None:
+
+        """
+        
+        Dumps the new values for the settings tab into the kijiku_settings.json file.
+
+        Parameters:
+        new_values (typing.List[typing.Tuple[str,str]]) : A list of tuples containing the key and value to be updated.
+
+        """
+
+        with open(GuiJsonUtil.current_kijiku_rules, "r") as file:
+            data = json.load(file)
+
+        for key, value in new_values:
+            data["open ai settings"][key] = value
+
+        with open(GuiJsonUtil.current_kijiku_rules, "w") as file:
+            json.dump(data, file)

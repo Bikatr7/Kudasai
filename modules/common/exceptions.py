03c21879d59d435b9fa9eed47194138d8cc8e426
Bikatr7 <Tetralon07@gmail.com>
2024-02-16 13:40:56 -0700

    Add InvalidAPIKeyException to common exceptions module

--- modules/common/exceptions.py
+++ modules/common/exceptions.py
@@ -14,3 +14,24 @@ class MaxBatchDurationExceededException(Exception):
     """
 
     pass
+
+##-------------------start-of-InvalidAPIKeyException--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class InvalidAPIKeyException(Exception):
+
+    """
+
+    InvalidAPIKeyException is an exception that is raised when the API key is invalid.
+
+    """
+
+    def __init__(self, model_name:str) -> None:
+
+        """
+
+        Parameters:
+        model_name (string) : The name of the model that the API key is invalid for.
+
+        """
+
+        self.message = f"The API key is invalid for the model {model_name}."

30efc4b2214d1435c4a31a8101c1cc5beb26a9e4
Bikatr7 <Tetralon07@gmail.com>
2024-04-07 23:09:26 -0600

    removed allowed models from file_ensurer since they exist in easytl

--- modules/common/file_ensurer.py
+++ modules/common/file_ensurer.py
@@ -105,39 +105,6 @@ class FileEnsurer():
         "gemini_max_output_tokens": None
     }
 }
-
-
-    ALLOWED_OPENAI_MODELS  = [
-        "gpt-3.5-turbo",
-        "gpt-4",
-        "gpt-4-turbo-preview",
-        "gpt-3.5-turbo-0301",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-0125",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-3.5-turbo-1106",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        "gpt-4-1106-preview",
-        "gpt-4-0125-preview"
-    ]
-
-    ALLOWED_GEMINI_MODELS = [
-        "gemini-1.0-pro-001",
-        "gemini-1.0-pro-vision-001",
-        "gemini-1.0-pro",
-        "gemini-1.0-pro-vision",
-        "gemini-1.0-pro-latest",
-        "gemini-1.0-pro-vision-latest",
-        "gemini-1.5-pro-latest",
-        "gemini-1.0-ultra-latest",
-        "gemini-pro",
-        "gemini-pro-vision",
-        "gemini-ultra"
-    ]
-
     INVALID_KIJIKU_RULES_PLACEHOLDER = {
     "INVALID JSON": 
     {

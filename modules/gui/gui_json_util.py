f9114aa7410358313fb61da320e3dc396997e3aa
Bikatr7 <Tetralon07@gmail.com>
2024-05-13 22:12:23 -0600

    fix gui_json_util.py

--- modules/gui/gui_json_util.py
+++ modules/gui/gui_json_util.py
@@ -12,7 +12,7 @@ from handlers.json_handler import JsonHandler
 
 class GuiJsonUtil:
 
-    current_kijiku_rules = dict()
+    current_translation_settings = dict()
 
 ##-------------------start-of-fetch_kijiku_setting_key_values()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -21,7 +21,7 @@ class GuiJsonUtil:
         
         """
         
-        Fetches the default values for the settings tab from the kijiku_settings.json file.
+        Fetches the default values for the settings tab from the translation_settings.json file.
 
         Parameters:
         key_name (str) : Which value to fetch.
@@ -32,16 +32,16 @@ class GuiJsonUtil:
         """
 
         ## Done this way because if the value is None, it'll be shown as a blank string in the settings tab, which is not what we want.
-        return GuiJsonUtil.current_kijiku_rules[header].get(key_name, "None")
+        return GuiJsonUtil.current_translation_settings[header].get(key_name, "None")
     
 ##-------------------start-of-update_kijiku_settings_with_new_values()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def update_kijiku_settings_with_new_values(gradio_kijiku_rule:gr.File, new_values:typing.List[typing.Tuple[str,str]]) -> None:
+    def update_kijiku_settings_with_new_values(gradio_translation_settings:gr.File, new_values:typing.List[typing.Tuple[str,str]]) -> None:
 
         """
         
-        Dumps the new values for the settings tab into the kijiku_settings.json file.
+        Dumps the new values for the settings tab into the translation_settings.json file.
 
         Parameters:
         new_values (typing.List[typing.Tuple[str,str]]) : A list of tuples containing the key and value to be updated.
@@ -49,7 +49,7 @@ class GuiJsonUtil:
         """
 
         ## save old json in case of need to revert
-        old_rules = GuiJsonUtil.current_kijiku_rules
+        old_rules = GuiJsonUtil.current_translation_settings
         new_rules = old_rules.copy()
 
         try:
@@ -58,32 +58,32 @@ class GuiJsonUtil:
                 for key, value in new_values:
                     new_rules[header][key] = JsonHandler.convert_to_correct_type(key, str(value))
 
-            JsonHandler.current_kijiku_rules = new_rules
+            JsonHandler.current_translation_settings = new_rules
             JsonHandler.validate_json()
 
             ## validate_json() sets a dict to the invalid placeholder if it's invalid, so if it's that, it's invalid
-            assert JsonHandler.current_kijiku_rules != FileEnsurer.INVALID_KIJIKU_RULES_PLACEHOLDER
+            assert JsonHandler.current_translation_settings != FileEnsurer.INVALID_TRANSLATION_SETTINGS_PLACEHOLDER
 
-            ## so, because of how gradio deals with temp file, we need to both dump into the settings file from FileEnsurer AND the gradio_kijiku_rule file which is stored in the temp folder under AppData
+            ## so, because of how gradio deals with temp file, we need to both dump into the settings file from FileEnsurer AND the gradio_translation_settings file which is stored in the temp folder under AppData
             ## name is the path to the file btw
-            with open(FileEnsurer.config_kijiku_rules_path, "w") as file:
+            with open(FileEnsurer.config_translation_settings_path, "w") as file:
                 json.dump(new_rules, file)
 
-            with open(gradio_kijiku_rule.name, "w") as file: ## type: ignore
+            with open(gradio_translation_settings.name, "w") as file: ## type: ignore
                 json.dump(new_rules, file)
 
-            GuiJsonUtil.current_kijiku_rules = new_rules
+            GuiJsonUtil.current_translation_settings = new_rules
 
         except Exception as e:
 
             ## revert to old data
-            with open(FileEnsurer.config_kijiku_rules_path, "w") as file:
+            with open(FileEnsurer.config_translation_settings_path, "w") as file:
                 json.dump(old_rules, file)
 
-            with open(gradio_kijiku_rule.name, "w") as file: ## type: ignore
+            with open(gradio_translation_settings.name, "w") as file: ## type: ignore
                 json.dump(old_rules, file)
 
-            GuiJsonUtil.current_kijiku_rules = old_rules
+            GuiJsonUtil.current_translation_settings = old_rules
 
             ## throw error so webgui can tell user
             raise e
